c8bd436fbb09dcee3d2ab20bc208d0bf833e7767
Aarush Agarwal <aarusha870@gmail.com>
2023-01-30 12:35:24 +0530

    Update GRPA1.py

--- week2/GRPA1.py
+++ week2/GRPA1.py
@@ -1,5 +1,5 @@
-*/Accept three positive integers as input and check if they form the sides of a right triangle. Print YES if they form one, and NO if they do not. The input will have three lines, with one integer on each line. The output should be a single line containing one of these two strings: YES or NO./*
- a=int(input())
+#Accept three positive integers as input and check if they form the sides of a right triangle. Print YES if they form one, and NO if they do not. The input will have three lines, with one integer on each line. The output should be a single line containing one of these two strings: YES or NO.
+a=int(input())
 b=int(input())
 c=int(input())
 if(a>b):
